766d29593a381248d36c88951271580e66b661cb
mingbogo <mingbogo@gmail.com>
2016-04-21 23:21:10 -0700

    Update resource_create
    Tested and succeeded. Having rcurl as required may be confusing for some CKAN users..

--- R/resource_create.R
+++ R/resource_create.R
@@ -4,7 +4,6 @@
 #'
 #' @param package_id (character) id of package that the resource should be added to.
 #' This should be an alphanumeric string. Required.
-#' @param rcurl (character) url of resource. Required.
 #' @param description (character) description (optional). Required.
 #' @param name (character) name (optional). Required.
 #' @param revision_id (character) revision id (optional)
@@ -46,7 +45,7 @@
 #'                    upload = file,
 #'                    rcurl = "http://google.com")
 #' }
-resource_create <- function(package_id = NULL, rcurl = NULL, revision_id = NULL, description = NULL,
+resource_create <- function(package_id = NULL, revision_id = NULL, description = NULL,
   format = NULL, hash = NULL, name = NULL, resource_type = NULL, mimetype = NULL,
   mimetype_inner = NULL, webstore_url = NULL, cache_url = NULL, size = NULL,
   created = NULL, last_modified = NULL, cache_last_updated = NULL,
@@ -54,7 +53,7 @@ resource_create <- function(package_id = NULL, rcurl = NULL, revision_id = NULL,
   url = get_default_url(), as = 'list', ...) {
 
   id <- as.ckan_package(package_id, url = url)
-  body <- cc(list(package_id = id$id, url = rcurl, revision_id = revision_id,
+  body <- cc(list(package_id = id$id, url = "", revision_id = revision_id,
                   description = description, format = format, hash = hash,
                   name = name, resource_type = resource_type, mimetype = mimetype,
                   mimetype_inner = mimetype_inner, webstore_url = webstore_url,
